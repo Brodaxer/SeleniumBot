aef3edef6fb48cd566bb0a79d865d27740d578b8
Brodaxer <mloszewski123@gmail.com>
2024-04-25 13:05:38 +0200

    done

--- cooookie.py
+++ cooookie.py
@@ -1,9 +1,10 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 
-timeout = time.time() + 60*1
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+timeout = time.time() + 60 * 5
+five_sec = time.time() + 5
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -13,42 +14,36 @@ driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
 click_cookie = driver.find_element(By.ID, value="cookie")
 store = driver.find_element(By.ID, value="store")
-monis = store.find_elements(By.TAG_NAME, value="b")
 cash = driver.find_element(By.ID, value="money")
 
-upgrades_list = []
-def check_for_upgrades():
-    for id_name, moni in zip(store.find_elements(By.TAG_NAME, value='div'), monis):
-        if len(moni.text.split()) > 0:
-            mon = moni.text.split()[-1].replace(",", "")
-            id = id_name.get_attribute("id")
-            # print(f"{id}-{mon}")
-            upgrades_list.append({id: int(mon)})
+list_of_upgrades = []
+
+def initialize_up_list():
+    # for id_name, moni in zip(store.find_elements(By.TAG_NAME, value='div'), store.find_elements(By.TAG_NAME, value="b")):
+    for id_name in store.find_elements(By.TAG_NAME, value='div'):
+        list_of_upgrades.append(id_name.get_attribute("id"))
 
 
-
-
-
-def cash_status():
-    cash1 = int(cash.text.replace(",", ""))
-    for ele in upgrades_list:
-        for x in ele:
-            if cash1 > ele[x]:
-                print(f"up available {cash1} ")
-            else:
-                print(f"nope {cash1} need {ele[x]}")
-
-
+initialize_up_list()
+list_of_upgrades.pop()
 while True:
-    time.sleep(0.5)
-    check_for_upgrades()
-    upgrades_list.reverse()
-    cash_status()
-    test = 0
-    if test == 5 or time.time() > timeout:
-        break
-    test = test -1
     click_cookie.click()
+    time.sleep(0.1)
+    if time.time() > five_sec:
+        cash1 = int(cash.text.replace(",", ""))
+        for ele in reversed(list_of_upgrades):
+            upgrade_cost = int(driver.find_element(By.ID, value=ele).find_element(By.TAG_NAME, value='b').text.split()[-1].replace(",", ""))
+            if cash1 >= upgrade_cost:
+                driver.find_element(By.ID, value=ele).click()
+                break
+    test = 0
+    click_cookie.click()
+    if test == 5 or time.time() > timeout:
+        print(driver.find_element(By.ID, value="cps").text)
+        break
+    test = test - 1
+    click_cookie.click()
+
 
 
 driver.quit()
